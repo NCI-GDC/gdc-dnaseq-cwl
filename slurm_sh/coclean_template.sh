6e6d4ae5e5c144f7599c2b30202c2c9b721a2e8c
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-01-22 11:39:48 -0600

    whitespace

--- slurm_sh/coclean_template.sh
+++ slurm_sh/coclean_template.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-###SBATCH --nodes=1
-###SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
 
-bam_url_array=("$@")
+bam_url_array="$@"
 echo ${bam_url_array}
 KNOWN_INDEL_VCF="Homo_sapiens_assembly38.known_indels.vcf.gz"
 KNOWN_SNP_VCF="dbsnp_144.hg38.vcf.gz"
@@ -11,7 +11,6 @@ THREAD_COUNT=8
 CWL_PATH="${HOME}/cocleaning-cwl/workflows/coclean/coclean_workflow.cwl.yaml"
 S3_INDEX_BUCKET="s3://bioinformatics_scratch/coclean"
 
-cd ${HOME}
 
 #make index dir
 INDEX_DIR="/mnt/SCRATCH/coclean_index"
@@ -32,6 +31,7 @@ s3cmd -c ~/.s3cfg.cleversafe --skip-existing get ${S3_INDEX_BUCKET}/${KNOWN_INDE
 #make BAM dir
 NEW_UUID=$(cat /dev/urandom | tr -dc 'A-Z0-9' | fold -w 32 | head -n1)
 DATA_DIR="/mnt/SCRATCH/data_"${NEW_UUID}
+DATA_DIR="/mnt/SCRATCH/data_TDOUNHI5AWD473MMGES5H5F6JHJ6BJLA"
 echo "DATA_DIR=${DATA_DIR}"
 mkdir -p ${DATA_DIR}
 
@@ -40,7 +40,7 @@ mkdir -p ${DATA_DIR}
 cd ${DATA_DIR}
 for bam_url in ${bam_url_array}
 do
-    s3cmd -c ~/.s3cfg.cleversafe get ${bam_url}
+    s3cmd -c ~/.s3cfg.cleversafe --skip-existing get ${bam_url}
 done
 
 
@@ -50,7 +50,7 @@ mkdir -p ${COCLEAN_DIR}
 
 
 # setup cwl command
-CWL_COMMAND="cwltool --debug --leave-tmpdir --outdir ${COCLEAN_DIR} ${CWL_PATH} --reference_fasta_path ${INDEX_DIR}/${REFERENCE_GENOME}.fa --uuid ${UUID} --known_indel_vcf_path ${INDEX_DIR}/${KNOWN_INDEL_VCF} --known_snp_vcf_path ${INDEX_DIR}/${KNOWN_SNP_VCF}  --thread_count ${THREAD_COUNT}"
+CWL_COMMAND="cwltool --debug --leave-tmpdir --outdir ${COCLEAN_DIR} ${CWL_PATH} --reference_fasta_path ${INDEX_DIR}/${REFERENCE_GENOME}.fa --uuid ${UUID} --known_indel_vcf_path ${INDEX_DIR}/${KNOWN_INDEL_VCF} --known_snp_vcf_path ${INDEX_DIR}/${KNOWN_SNP_VCF} --thread_count ${THREAD_COUNT}"
 for bam_url in ${bam_url_array}
 do
     bam_name=$(basename ${bam_url})
@@ -58,7 +58,7 @@ do
     CWL_COMMAND="${CWL_COMMAND} --bam_path ${bam_path}"
 done
 
-cd ${HOME}
+cd ${COCLEAN_DIR}
 
 # run cwl
 workon p2
