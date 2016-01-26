2146c0b9a9b43bdf1ad39461e30eb7db71422f49
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-01-26 13:11:32 -0600

    init insert into postgres script to create rows of case_id and gdc_id

---
+++ slurm_sh/insert_casegdc_from_slurm.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+
+
+
+import argparse
+import glob
+import logging
+import os
+import sys
+
+from cdis_pipe_utils import pipe_util
+
+
+def main():
+    parser = argparse.ArgumentParser('insert rows into table based on slurm scripts')
+    # Logging flags.
+    parser.add_argument('-d', '--debug',
+        action = 'store_const',
+        const = logging.DEBUG,
+        dest = 'level',
+        help = 'Enable debug logging.',
+    )
+    parser.set_defaults(level = logging.INFO)
+
+    parser.add_argument('--slurm_script_dir',
+                        required = True
+    )
+    parser.add_argument('--psql_username',
+                        required = True
+    )
+    parser.add_argument('--psql_password',
+                        required = True
+    )
+    parser.add_argument('--uuid',
+                        required = True
+    )
+
+    args = parser.parse_args()
+    slurm_script_dir = args.slurm_script_dir
+    psql_username = args.psql_username
+    psql_password = args.psql_password
+    psql_host = args.psql_host
+    uuid = args.uuid
+
+    slurm_script_list = get_slurm_script_list(slurm_script_dir)
+    tool_name = 'insert_casegdc_from_slurm'
+    logger = pipe_util.setup_logging(tool_name, args, uuid)
+
+    engine = pipe_util.setup_db_with_postgres(uuid, username=psql_username, password = psql_password,
+                                              host = psql_host, port = psql_port, db = psql_db)
+
+
+if __name__=='__main__':
+    main()
