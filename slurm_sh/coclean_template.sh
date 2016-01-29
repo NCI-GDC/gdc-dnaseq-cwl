48cdb4e319872ec4400b88f0aea93f3441c4f12c
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-01-29 16:24:45 -0600

    use more variables

--- slurm_sh/coclean_template.sh
+++ slurm_sh/coclean_template.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --workdir=/mnt/scratch/
 
-
+SCRATCH_DIR="/mnt/scratch"
 BAM_URL_ARRAY="XX_BAM_URL_ARRAY_XX"
 CASE_ID="XX_CASE_ID_XX"
 UUID=${CASE_ID}
@@ -14,17 +14,18 @@ KNOWN_INDEL_VCF="Homo_sapiens_assembly38.known_indels.vcf.gz"
 KNOWN_SNP_VCF="dbsnp_144.hg38.vcf.gz"
 REFERENCE_GENOME="GRCh38.d1.vd1"
 THREAD_COUNT=40
-COCLEAN_WORKFLOW_PATH="${HOME}/cocleaning-cwl/workflows/coclean/coclean_workflow.cwl.yaml"
-BUILDBAMINDEX_TOOL_PATH="${HOME}/cocleaning-cwl/tools/picard_buildbamindex.cwl.yaml"
 S3_INDEX_BUCKET="s3://bioinformatics_scratch/coclean"
 S3_OUT_BUCKET="s3://tcga_exome_blca_coclean"
 S3_LOG_BUCKET="s3://tcga_exome_blca_coclean_log"
 S3_CWL_PATH="s3://bioinformatics_scratch/cocleaning-cwl.tar.gz"
 
-INDEX_DIR="/mnt/scratch/coclean_index"
-DATA_DIR="/mnt/scratch/data_"${CASE_ID}
+INDEX_DIR="${SCRATCH_DIR}/coclean_index"
+DATA_DIR="${SCRATCH_DIR}/data_"${CASE_ID}
 COCLEAN_DIR=${DATA_DIR}/coclean
-
+COCLEAN_WORKFLOW_PATH="${COCLEAN_DIR}/cocleaning-cwl/workflows/coclean/coclean_workflow.cwl.yaml"
+BUILDBAMINDEX_TOOL_PATH="${COCLEAN_DIR}/cocleaning-cwl/tools/picard_buildbamindex.cwl.yaml"
+S3_CFG=${HOME}/.s3cfg.cleversafe
+CWL_RUNNER=${HOME}/.virtualenvs/p2_${CASE_ID}/bin/cwltool
 
 function install_virtenv()
 {
@@ -32,13 +33,13 @@ function install_virtenv()
     pip install virtualenvwrapper --user
     source ${HOME}/.local/bin/virtualenvwrapper.sh
     mkvirtualenv --python /usr/bin/python2 p2_${CASE_ID}
+    source ${HOME}/.virtualenvs/p2_${CASE_ID}/bin/activate
     pip install --upgrade pip
 }
 
 function install_cwltool()
 {
     export http_proxy=http://cloud-proxy:3128; export https_proxy=http://cloud-proxy:3128;
-    source ${HOME}/.virtualenvs/p2/bin/activate
     pip install -r ${REQUIREMENTS_PATH}
 }
 
@@ -52,7 +53,7 @@ install_cwltool
 #cget cwl
 cd ${DATA_DIR}
 cwl_tarball=$(basename ${S3_CWL_PATH})
-s3cmd -c ~/.s3cfg.cleversafe --force get ${S3_CWL_PATH}
+s3cmd -c ${S3_CFG} --force get ${S3_CWL_PATH}
 tar xf ${cwl_tarball}
 
 
@@ -62,13 +63,13 @@ cd ${INDEX_DIR}
 
 
 # get index files, if needed
-s3cmd -c ~/.s3cfg.cleversafe --skip-existing get ${S3_INDEX_BUCKET}/${REFERENCE_GENOME}.dict
-s3cmd -c ~/.s3cfg.cleversafe --skip-existing get ${S3_INDEX_BUCKET}/${REFERENCE_GENOME}.fa
-s3cmd -c ~/.s3cfg.cleversafe --skip-existing get ${S3_INDEX_BUCKET}/${REFERENCE_GENOME}.fa.fai
-s3cmd -c ~/.s3cfg.cleversafe --skip-existing get ${S3_INDEX_BUCKET}/${KNOWN_SNP_VCF}
-s3cmd -c ~/.s3cfg.cleversafe --skip-existing get ${S3_INDEX_BUCKET}/${KNOWN_SNP_VCF}.tbi
-s3cmd -c ~/.s3cfg.cleversafe --skip-existing get ${S3_INDEX_BUCKET}/${KNOWN_INDEL_VCF}
-s3cmd -c ~/.s3cfg.cleversafe --skip-existing get ${S3_INDEX_BUCKET}/${KNOWN_INDEL_VCF}.tbi
+s3cmd -c ${S3_CFG} --skip-existing get ${S3_INDEX_BUCKET}/${REFERENCE_GENOME}.dict
+s3cmd -c ${S3_CFG} --skip-existing get ${S3_INDEX_BUCKET}/${REFERENCE_GENOME}.fa
+s3cmd -c ${S3_CFG} --skip-existing get ${S3_INDEX_BUCKET}/${REFERENCE_GENOME}.fa.fai
+s3cmd -c ${S3_CFG} --skip-existing get ${S3_INDEX_BUCKET}/${KNOWN_SNP_VCF}
+s3cmd -c ${S3_CFG} --skip-existing get ${S3_INDEX_BUCKET}/${KNOWN_SNP_VCF}.tbi
+s3cmd -c ${S3_CFG} --skip-existing get ${S3_INDEX_BUCKET}/${KNOWN_INDEL_VCF}
+s3cmd -c ${S3_CFG} --skip-existing get ${S3_INDEX_BUCKET}/${KNOWN_INDEL_VCF}.tbi
 
 
 #make BAM dir
@@ -79,7 +80,7 @@ mkdir -p ${DATA_DIR}
 cd ${DATA_DIR}
 for bam_url in ${bam_url_array}
 do
-    s3cmd -c ~/.s3cfg.cleversafe --force get ${bam_url}
+    s3cmd -c ${S3_CFG} --force get ${bam_url}
 done
 
 
@@ -90,7 +91,7 @@ do
     bam_name=$(basename ${bam_url})
     bam_path=${DATA_DIR}/${bam_name}
     CWL_COMMAND="--debug --leave-tmpdir --outdir ${DATA_DIR} ${BUILDBAMINDEX_TOOL_PATH} --uuid ${UUID} --input_bam ${bam_path}"
-    ${HOME}/.virtualenvs/p2/bin/cwltool ${CWL_COMMAND}
+    ${CWL_RUNNER} ${CWL_COMMAND}
 done
 
 
@@ -126,12 +127,12 @@ do
     bai_file="${bam_base}.bai"
     bam_path=${COCLEAN_DIR}/${bam_file}
     bai_path=${COCLEAN_DIR}/${bai_file}
-    echo "uploading: s3cmd -c ~/.s3cfg.cleversafe put ${bai_path} ${S3_OUT_BUCKET}/${gdc_id}/"
-    s3cmd -c ~/.s3cfg.cleversafe put ${bai_path} ${S3_OUT_BUCKET}/${gdc_id}/
-    echo "uploading: s3cmd -c ~/.s3cfg.cleversafe put ${bam_path} ${S3_OUT_BUCKET}/${gdc_id}/"
-    s3cmd -c ~/.s3cfg.cleversafe put ${bam_path} ${S3_OUT_BUCKET}/${gdc_id}/
+    echo "uploading: s3cmd -c ${S3_CFG} put ${bai_path} ${S3_OUT_BUCKET}/${gdc_id}/"
+    s3cmd -c ${S3_CFG} put ${bai_path} ${S3_OUT_BUCKET}/${gdc_id}/
+    echo "uploading: s3cmd -c ${S3_CFG} put ${bam_path} ${S3_OUT_BUCKET}/${gdc_id}/"
+    s3cmd -c ${S3_CFG} put ${bam_path} ${S3_OUT_BUCKET}/${gdc_id}/
 done
-s3cmd -c ~/.s3cfg.cleversafe put ${COCLEAN_DIR}/${CASE_ID}.db ${S3_LOG_BUCKET}/
+s3cmd -c ${S3_CFG} put ${COCLEAN_DIR}/${CASE_ID}.db ${S3_LOG_BUCKET}/
 
 #cleanup
 rm -rf ${DATA_DIR}
