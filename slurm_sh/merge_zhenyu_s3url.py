ff5ecfc9f67a927a157201c25bf75129a3299bc2
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-02-11 19:21:25 -0600

    use db_cred_url

--- slurm_sh/merge_zhenyu_s3url.py
+++ slurm_sh/merge_zhenyu_s3url.py
@@ -2,8 +2,8 @@
 
 ###SQL command###
 #prod_bioinfo=> \o out.txt
-#prod_bioinfo=> select * from harmonized_files where experimental_strategy = 'WXS' and project = 'BLCA' and docker_tag = 'pipeline:gdc0.513' order by case_id, filename;
 #prod_bioinfo=> select * from harmonized_files where experimental_strategy = 'WXS' and project = 'BLCA' order by case_id, filename;
+#prod_bioinfo=> select * from wxs_coclean_participantid_gdcid where study = 'TCGA' and project = 'BLCA' order by case_id, filename;
 #prod_bioinfo=> \q
 ###
 
@@ -17,7 +17,8 @@ import sys
 
                 
 
-def write_case_file(template_file, caseid, qcpass_case_bamurl_dict, scratch_dir, thread_count, cwl_git_hash):
+def write_case_file(template_file, caseid, case_bamurl_dict, scratch_dir, thread_count, git_cwl_hash,
+                    db_cred_url, s3_cfg_path):
     template_dir = os.path.dirname(template_file)
     out_dir = os.path.join(template_dir, 'case_slurm_sh')
     os.makedirs(out_dir, exist_ok=True)
@@ -40,39 +41,46 @@ def write_case_file(template_file, caseid, qcpass_case_bamurl_dict, scratch_dir,
             elif 'XX_THREAD_COUNT_XX' in line:
                 newline = line.replace('XX_THREAD_COUNT_XX', thread_count)
                 out_path_open.write(newline)
-            elif 'XX_POSTGRES_USERNAME_XX' in line:
-                newline = line.replace('XX_POSTGRES_USERNAME_XX', db_username)
+            elif 'XX_DB_CRED_URL_XX' in line:
+                newline = line.replace('XX_DB_CRED_URL_XX', db_cred_url)
                 out_path_open.write(newline)
-            elif 'XX_POSTGRES_PASSWORD_XX' in line:
-                newline = line.replace('XX_POSTGRES_PASSWORD_XX', db_password)
+            elif 'XX_S3_CFG_PATH_XX' in line:
+                newline = line.replace('XX_S3_CFG_PATH_XX', s3_cfg_path)
                 out_path_open.write(newline)
-            elif 'XX_CWL_GIT_HASH_XX' in line:
-                newline = line.replace('XX_CWL_GIT_HASH_XX', cwl_git_hash)
+            elif 'XX_GIT_CWL_HASH_XX' in line:
+                newline = line.replace('XX_GIT_CWL_HASH_XX', git_cwl_hash)
                 out_path_open.write(newline)
             else:
                 out_path_open.write(line)
     out_path_open.close()
     return
 
-                
-
-def get_case_gdcid_dict(zhenyu_file, zhenyu_program, zhenyu_project):
-    case_gdcid_dict = dict()
-    f_open = open(zhenyu_file, 'rt')
-    reader = csv.reader(f_open, quotechar='"', delimiter=',',quoting=csv.QUOTE_ALL, skipinitialspace=True)
+def get_gdcid_bamurl_dict(harmonized_file):
+    gdcid_bamurl_dict = dict()
+    f_open = open(harmonized_file, 'rt')
+    reader = csv.reader(f_open, quotechar='"', delimiter='|',quoting=csv.QUOTE_ALL, skipinitialspace=True)
     for line_split in reader:
-        program_id = line_split[0]
-        project_id = line_split[1]
+        gdc_id = line_split[0]
+        aliquot_id = line_split[1]
+        
+
+def get_caseid_gdcid_dict(zhenyu_file):
+    caseid_gdcid_dict = dict()
+    f_open = open(zhenyu_file, 'rt')
+    reader = csv.reader(f_open, quotechar='"', delimiter='|',quoting=csv.QUOTE_ALL, skipinitialspace=True)
+    for line_split in reader:
+        study = line_split[0]
+        disease = line_split[1]
         case_id = line_split[2]
-        gdc_ids = line_split[3]
-        good_case = line_split[4]
-        if program_id == 'program':
-            continue
+        participant_id = line_split[3]
+        gdc_id = line_split[4]
+        sample_type = line_split[5]
+        if case_id in caseid_gdcid_dict.keys():
+            caseid_gdcid_dict[case_id].add(gdc_id)
         else:
-            if zhenyu_program == program_id and zhenyu_project == project_id and good_case == 'TRUE':
-                gdcid_list = gdc_ids.split(',').strip()
-                case_gdcid_dict[case_id] = sorted(gdcid_list)
-    return case_gdcid_dict
+            caseid_gdcid_dict[case_id]=set()
+            caseid_gdcid_dict[case_id].add(gdc_id)
+    return caseid_gdcid_dict
 
 def main():
     parser = argparse.ArgumentParser('make slurm')
@@ -85,17 +93,11 @@ def main():
     )
     parser.set_defaults(level = logging.INFO)
 
-    parser.add_argument('--sql_file',
+    parser.add_argument('--harmonized_file',
                         required = True,
                         help = 'pulled from harmonized_files'
     )
     parser.add_argument('--zhenyu_file',
-                        required = True
-    )
-    parser.add_argument('--zhenyu_program',
-                        required = True
-    )
-    parser.add_argument('--zhenyu_project',
                         required = True
     )
     parser.add_argument('--template_file',
@@ -108,36 +110,34 @@ def main():
     parser.add_argument('--thread_count',
                         required = True
     )
-    parser.add_argument('--db_username',
+    parser.add_argument('--db_cred_url',
                         required = True
     )
-    parser.add_argument('--db_password',
+    parser.add_argument('--s3_cfg_path',
                         required = True
     )
-    parser.add_argument('--cwl_git_hash',
+    parser.add_argument('--git_cwl_hash',
                         required = True
     )
 
     args = parser.parse_args()
-    sql_file = args.sql_file
+    harmonized_file = args.sql_file
     zhenyu_file = args.zhenyu_file
-    zhenyu_program = args.zhenyu_program
-    zhenyu_project = args.zhenyu_project
     template_file = args.template_file
     scratch_dir = args.scratch_dir
     thread_count = args.thread_count
-    db_username = args.db_username
-    db_password = args.db_password
-    cwl_git_hash = args.cwl_git_hash
+    db_cred_url = args.db_cred_url
+    s3_cfg_path = args.s3_cfg_path
+    git_cwl_hash = args.git_cwl_hash
     #uuid = 'a_uuid'
     #tool_name = 'create_slurm_from_template'
     #logger = pipe_util.setup_logging(tool_name, args, uuid)
 
-    case_gdcid_dict = get_case_gdcid_dict(zhenyu_file, zhenyu_program, zhenyu_project)    
-    gdcid_bamurl_dict = get_gdcid_bamurl_dict(sql_file)
+    case_gdcid_dict = get_caseid_gdcid_dict(zhenyu_file)    
+    gdcid_bamurl_dict = get_gdcid_bamurl_dict(harmonized_file)
     case_bamurl_dict = join_case_bamurl(case_bamurl_dict, keep_bamurl_set)
     
     for caseid in sorted(list(case_bamurl_dict.keys())):
-        write_case_file(template_file, caseid, qcpass_case_bamurl_dict, scratch_dir, thread_count, db_username, db_password, cwl_git_hash)
+        write_case_file(template_file, caseid, case_bamurl_dict, scratch_dir, thread_count, git_cwl_hash, db_cred_url, s3_cfg_path)
 if __name__=='__main__':
     main()
