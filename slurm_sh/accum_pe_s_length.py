a0a2ce5a985bed7986af2285e5cdf9e5cefff7c3
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-06-13 18:55:32 -0500

    proper path to db creds

--- slurm_sh/accum_pe_s_length.py
+++ slurm_sh/accum_pe_s_length.py
@@ -4,13 +4,18 @@ import os
 import subprocess
 
 def has_fastqc_data_Basic_Statistics(db_file):
+    print('has_fastqc_data_Basic_Statistics()')
     cmd = ['sqlite3', db_file, "'.tables'"]
     shell_cmd = ' '.join(cmd)
-    output = subprocess.check_output(cmd, shell=True)
+    print('shell_cmd=%s' % shell_cmd)
+    output = subprocess.check_output(shell_cmd, shell=True)
     output_split = output.decode().split('\n')
+    print('output_split=%s' % output_split)
     for table in output_split:
-        if 'fastqc_data_Basic_Statistics' in table.strip():
+        if 'fastqc_data_Basic_Statistics' in table.encode('ascii').strip():
+            print('db_file %s has fastqc_data_Basic_Statistic')
             return True
+    print('db_file %s Does Not have fastqc_data_Basic_Statistic')
     return False
 
 def to_postgres(db_file):
@@ -18,7 +23,7 @@ def to_postgres(db_file):
     print('\tdb_file=%s' % db_file)
     cmd = ['cwltool', '--debug', '--enable-net', '--custom-net', 'host',
            '/home/ubuntu/cocleaning-cwl/tools/sqlite_to_postgres_hirate.cwl.yaml', '--source_sqlite_path', db_file,
-           '--postgres_creds_path', 'connect_jhsavage_test.ini', '--ini_section', 'test', '--uuid', 'stop']
+           '--postgres_creds_path', '/home/ubuntu/connect_jhsavage_test.ini', '--ini_section', 'test', '--uuid', 'stop']
     print('cmd=\n%s' % cmd)
     output = subprocess.check_output(cmd)
     print('db_file:\t%s' % db_file)
@@ -46,8 +51,9 @@ def main():
 
     for s3_url in s3_list:
         print('s3_url=%s' % s3_url)
-        cmd = ['s3cmd', '-c', '/home/ubuntu/.s3cfg.cleversafe', 'get', s3_url]
+        cmd = ['s3cmd', '-c', '/home/ubuntu/.s3cfg.cleversafe', 'get', s3_url.encode('ascii')]
         print('cmd=%s' % cmd)
+        output = subprocess.check_output(cmd)
         db_file = s3_url.split('/')[-1]
         print('db_file=%s' % db_file)
         write_data(db_file)
