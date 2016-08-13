aaf31774dceb8bdbb8d673f561c0b4c57fc194fb
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-08-13 18:48:43 -0500

    update notes

--- workflows/markduplicates/salt.sh
+++ workflows/markduplicates/salt.sh
@@ -22,3 +22,11 @@ salt -G 'cluster_name:DEADPOOL' cmd.run 'apt-get update &&  apt-get install pyth
 salt -G 'cluster_name:DEADPOOL' cmd.run runas=ubuntu 'rm -rf /home/ubuntu/cocleaning-cwl /home/ubuntu/md_json /mnt/SCRATCH/cache /mnt/SCRATCH/tmp'
 
 salt -G 'cluster_name:DEADPOOL' cmd.run runas=ubuntu 'docker rmi -f $(docker images -q)'
+
+#stage
+salt -G 'cluster_name:DEADPOOL' cmd.run runas=ubuntu 'docker rmi -f $(docker images -q)'
+salt -G 'cluster_name:DEADPOOL' cmd.run runas=ubuntu 'rm -rf /mnt/SCRATCH/cache /mnt/SCRATCH/tmp'
+salt -G 'cluster_name:DEADPOOL' cmd.run 'apt-get update &&  apt-get install python-dev libffi-dev libssl-dev htop s3cmd -y && apt-get clean'
+salt -G 'cluster_name:DEADPOOL' cmd.run 'echo "172.17.29.16 signpost.service.consul" >> /etc/hosts'
+salt -G 'cluster_name:DEADPOOL' cmd.run runas=ubuntu 'sed -i "s/\/usr\/local\/bin\/virtualenvwrapper.sh/\/usr\/share\/virtualenvwrapper\/virtualenvwrapper.sh/g" /home/ubuntu/.bashrc'
+salt -G 'cluster_name:DEADPOOL' cmd.run runas=ubuntu 'bash -c "export http_proxy=http://cloud-proxy:3128 && export https_proxy=http://cloud-proxy:3128 && source /usr/share/virtualenvwrapper/virtualenvwrapper.sh && mkvirtualenv --python /usr/bin/python2 cwl && pip install --upgrade pip && pip install 'requests[security]' && pip install cwltool --no-cache-dir"'
