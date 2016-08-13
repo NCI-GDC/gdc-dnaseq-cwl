452dd6cb093a06e88d7b77cec8d06453b1e43106
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-08-13 18:49:51 -0500

    update

--- workflows/markduplicates/priority.sh
+++ workflows/markduplicates/priority.sh
@@ -2,7 +2,7 @@ cat qcfail_realign_table.txt | grep mkdup_needed > mkdup_needed.txt
 sort -k14V -t$'\t'  mkdup_needed.txt > mkdup_needed_sort.txt
 
 tail -n 96 mkdup_needed_sort.txt | awk '{print $3}' > deadpool.lst
-while read line ; do grep -l ${line} md_json/* ; done < deadpool.lst  > sbatch_deadpool.sh
+while read line ; do grep -l ${line} md_slurm/* ; done < deadpool.lst  > sbatch_deadpool.sh
 sed -i 's/^/sbatch /' sbatch_deadpool.sh
 
 
