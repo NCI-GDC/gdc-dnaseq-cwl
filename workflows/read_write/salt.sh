119c57d38217dfa8af9980495cf503ac4cac47f5
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-08-24 15:09:09 -0500

    update

--- workflows/read_write/salt.sh
+++ workflows/read_write/salt.sh
@@ -3,7 +3,7 @@ salt -G 'cluster_name:WOLVERINE' cmd.run runas=ubuntu 'rm -rf /mnt/SCRATCH/cache
 salt -G 'cluster_name:WOLVERINE' cmd.run 'apt-get update &&  apt-get install virtualenvwrapper python-dev libffi-dev libssl-dev htop s3cmd -y && apt-get clean'
 salt -G 'cluster_name:WOLVERINE' cmd.run 'echo "172.17.29.16 signpost.service.consul" >> /etc/hosts'
 salt -G 'cluster_name:WOLVERINE' cmd.run runas=ubuntu 'sed -i "s/\/usr\/local\/bin\/virtualenvwrapper.sh/\/usr\/share\/virtualenvwrapper\/virtualenvwrapper.sh/g" /home/ubuntu/.bashrc'
-salt -G 'cluster_name:WOLVERINE' cmd.run runas=ubuntu 'bash -c "export http_proxy=http://cloud-proxy:3128 && export https_proxy=http://cloud-proxy:3128 && source /usr/share/virtualenvwrapper/virtualenvwrapper.sh && mkvirtualenv --python /usr/bin/python2 cwl && pip install --upgrade pip && pip install 'requests[security]' && s3cmd -c ~/.s3cfg.ceph get s3://bioinformatics_scratch/1.0_gdc_c.tar.gz pip install 1.0_gdc_c.tar.gz --no-cache-dir"'
+salt -G 'cluster_name:WOLVERINE' cmd.run runas=ubuntu 'bash -c "export http_proxy=http://cloud-proxy:3128 && export https_proxy=http://cloud-proxy:3128 && source /usr/share/virtualenvwrapper/virtualenvwrapper.sh && mkvirtualenv --python /usr/bin/python2 cwl && pip install --upgrade pip && pip install 'requests[security]' && s3cmd -c ~/.s3cfg.ceph get --skip-existing s3://bioinformatics_scratch/1.0_gdc_c.tar.gz && pip install 1.0_gdc_c.tar.gz --no-cache-dir"'
 
 
 #check
