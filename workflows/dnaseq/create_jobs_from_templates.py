31f66c0fcd8e3c12ee973e8cbc7128a8c8fe2cfa
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-09-29 17:26:11 -0500

    new structure

--- workflows/dnaseq/create_jobs_from_templates.py
+++ workflows/dnaseq/create_jobs_from_templates.py
@@ -14,29 +14,54 @@ def read_header(header_line):
         header_key_dict[header_column] = i
     return header_key_dict
 
-
-def generate_bam_extract(location, write_path):
-    f_open = open(write_path,'w')
-    f_open.write('{\n  "urls": [\n    "' + location + '"\n  ]\n}')
-    f_open.close()
+def get_parameter_from_file_name(file_name, parameter):
+    import json
+    import requests
+    import uuid
+    endpoint = 'https://gdc-api.nci.nih.gov/legacy/files'
+    filter = {
+        "op": "in",
+        "content": {
+            "field": "file_name",
+            "value": [
+                file_name
+            ]
+        }
+    }
+    parameters = {'filters':json.dumps(filter)}
+    response = requests.get(endpoint, params = parameters)
+    if response.status_code == 200:
+        response_data = response.json()['data']
+        count = response_data['pagination']['count']
+        if count != 1:
+            print('response_data:%s' % response_data)
+            print('count != 1: %s' % count)
+            sys.exit(1)
+        else:
+            parameter_value = response_data['hits'][0][parameter]
+            return parameter_value
+    sys.exit(1)
     return
 
-
-def generate_etl(job_uuid, etl_json_template_path, alignment_last_step, s3_load_bucket, node_json_dir,
-                 job_signpost_json, write_path):
-    f_open = open(write_path, 'w')
-    with open(etl_json_template_path, 'r') as read_open:
+def generate_runner(bam_file_name, db_table_name, job_uuid, repo_hash, s3_load_bucket,
+                    json_template_path):
+    job_json = job_uuid + '.json'
+    f_open = open(job_json, 'w')
+    with open(json_template_path, 'r') as read_open:
         for line in read_open:
-            if 'XX_LAST_STEP_XX' in line:
-                newline = line.replace('XX_LAST_STEP_XX', alignment_last_step)
+            if 'XX_BAM_SIGNPOST_ID_XX' in line:
+                bam_signpost_id = get_parameter_from_file_name(bam_file_name, 'file_id')
+                newline = line.replace('XX_BAM_SIGNPOST_ID_XX', bam_signpost_id)
                 f_open.write(newline)
-            elif 'XX_BAM_SIGNPOST_JSON_XX' in line:
-                signpost_json_path = os.path.join(node_json_dir, job_signpost_json)
-                newline = line.replace('XX_BAM_SIGNPOST_JSON_XX', signpost_json_path)
+            elif 'XX_DB_TABLE_NAME_XX' in line:
+                newline = line.replace('XX_DB_TABLE_NAME_XX', db_table_name)
                 f_open.write(newline)
             elif 'XX_LOAD_BUCKET_XX' in line:
                 newline = line.replace('XX_LOAD_BUCKET_XX', s3_load_bucket)
                 f_open.write(newline)
+            elif 'XX_REPO_HASH_XX' in line:
+                newline = line.replace('XX_REPO_HASH_XX', repo_hash)
+                f_open.write(newline)
             elif 'XX_UUID_XX' in line:
                 newline = line.replace('XX_UUID_XX', job_uuid)
                 f_open.write(newline)
@@ -45,39 +70,29 @@ def generate_etl(job_uuid, etl_json_template_path, alignment_last_step, s3_load_
     f_open.close()
     return
 
-def generate_slurm(job_uuid, slurm_template_path, db_cred_path, scratch_dir, git_cwl_hash, s3_load_bucket, job_etl_json, node_json_dir,
-                   cghub_id, gdc_id, gdc_src_id, thread_count, write_path):
-    f_open = open(write_path, 'w')
+def generate_slurm(bam_file_name, db_table_name, job_uuid, node_json_dir, repo_hash, scratch_dir, 
+                   slurm_template_path):
+    job_slurm = job_uuid + '.sh'
+    f_open = open(job_slurm, 'w')
     with open(slurm_template_path, 'r') as read_open:
         for line in read_open:
-            if 'XX_CGHUB_ID_XX' in line:
-                newline = line.replace('XX_CGHUB_ID_XX', cghub_id)
+            if 'XX_BAM_SIGNPOST_ID_XX' in line:
+                bam_signpost_id = get_parameter_from_file_name(bam_file_name, 'file_id')
+                newline = line.replace('XX_BAM_SIGNPOST_ID_XX', bam_signpost_id)
                 f_open.write(newline)
-            elif 'XX_DB_CRED_PATH_XX' in line:
-                newline = line.replace('XX_DB_CRED_PATH_XX', db_cred_path)
+            elif 'XX_DB_TABLE_NAME_XX' in line:
+                newline = line.replace('XX_DB_TABLE_NAME_XX', db_table_name)
                 f_open.write(newline)
-            elif 'XX_ETL_JSON_PATH_XX' in line:
-                etl_json_path = os.path.join(node_json_dir, job_etl_json)
-                newline = line.replace('XX_ETL_JSON_PATH_XX', etl_json_path)
+            elif 'XX_JSON_PATH_XX' in line:
+                json_path = os.path.join(node_json_dir, job_uuid + ".json")
+                newline = line.replace('XX_JSON_PATH_XX', json_path)
                 f_open.write(newline)
-            elif 'XX_GDC_ID_XX' in line:
-                newline = line.replace('XX_GDC_ID_XX', gdc_id)
-                f_open.write(newline)
-            elif 'XX_GDC_SRC_ID_XX' in line:
-                newline = line.replace('XX_GDC_SRC_ID_XX', gdc_src_id)
-                f_open.write(newline)
-            elif 'XX_GIT_CWL_HASH_XX' in line:
-                newline = line.replace('XX_GIT_CWL_HASH_XX', git_cwl_hash)
-                f_open.write(newline)
-            elif 'XX_S3_LOAD_BUCKET_XX' in line:
-                newline = line.replace('XX_S3_LOAD_BUCKET_XX', s3_load_bucket)
+            elif 'XX_REPO_HASH_XX' in line:
+                newline = line.replace('XX_REPO_HASH_XX', repo_hash)
                 f_open.write(newline)
             elif 'XX_SCRATCH_DIR_XX' in line:
                 newline = line.replace('XX_SCRATCH_DIR_XX', scratch_dir)
                 f_open.write(newline)
-            elif 'XX_THREAD_COUNT_XX' in line:
-                newline = line.replace('XX_THREAD_COUNT_XX', thread_count)
-                f_open.write(newline)
             elif 'XX_UUID_XX' in line:
                 newline = line.replace('XX_UUID_XX', job_uuid)
                 f_open.write(newline)
@@ -86,22 +101,15 @@ def generate_slurm(job_uuid, slurm_template_path, db_cred_path, scratch_dir, git
     f_open.close()
     return
 
-
-def setup_job(db_cred_path, etl_json_template_path, git_cwl_hash, node_json_dir, s3_load_bucket, scratch_dir,
-              slurm_template_path, thread_count, alignment_last_step, cghub_id, gdc_id, gdc_src_id, location):
+def setup_job(bam_file_name, db_table_name, node_json_dir, repo_hash, s3_load_bucket, scratch_dir,
+              json_template_path, slurm_template_path):
     job_uuid = str(uuid.uuid4())
 
-    job_etl_json = job_uuid + '_etl.json'
-    job_signpost_json = job_uuid + '_signpost.json'
-    job_slurm = job_uuid + '.sh'
-
-    generate_bam_extract(location, job_signpost_json)
-    generate_etl(job_uuid, etl_json_template_path, alignment_last_step, s3_load_bucket, node_json_dir,
-                 job_signpost_json, job_etl_json)
-    generate_slurm(job_uuid, slurm_template_path, db_cred_path, scratch_dir, git_cwl_hash, s3_load_bucket, job_etl_json, node_json_dir,
-                   cghub_id, gdc_id, gdc_src_id, thread_count, job_slurm)
+    generate_runner(bam_file_name, db_table_name, job_uuid, repo_hash, s3_load_bucket,
+                    json_template_path)
+    generate_slurm(bam_file_name, db_table_name, job_uuid, node_json_dir, repo_hash, scratch_dir, 
+                   slurm_template_path)
     return
-
 
 def main():
     parser = argparse.ArgumentParser('make slurm')
@@ -114,19 +122,16 @@ def main():
     )
     parser.set_defaults(level = logging.INFO)
 
-    parser.add_argument('--db_cred_path',
+    parser.add_argument('--db_table_name',
                         required = True
     )
     parser.add_argument('--json_template_path',
                         required = True
     )
-    parser.add_argument('--git_cwl_hash',
-                        required = True
-    )
-    parser.add_argument('--job_table_path',
-                        required = True
-    )
     parser.add_argument('--node_json_dir',
+                        required = True
+    )
+    parser.add_argument('--repo_hash',
                         required = True
     )
     parser.add_argument('--s3_load_bucket',
@@ -141,15 +146,13 @@ def main():
 
     args = parser.parse_args()
 
-    db_cred_path = args.db_cred_path
+    db_table_name = args.db_table_name
     json_template_path = args.json_template_path
-    git_cwl_hash = args.git_cwl_hash
-    job_table_path = args.job_table_path
     node_json_dir = args.node_json_dir
+    repo_hash = args.repo_hash
     s3_load_bucket = args.s3_load_bucket
     scratch_dir = args.scratch_dir
     slurm_template_path = args.slurm_template_path
-    thread_count = args.thread_count
 
     with open(job_table_path, 'r') as job_table_open:
         for job_line in job_table_open:
@@ -157,15 +160,12 @@ def main():
                 header_key_dict = read_header(job_line)
             else:
                 job_split = job_line.strip().split('\t')
-                alignment_last_step = job_split[header_key_dict['alignment_last_step']]
-                cat = job_split[header_key_dict['cat']]
-                cghub_id = job_split[header_key_dict['cghub_id']]
-                gdc_id = job_split[header_key_dict['gdc_id']]
-                gdc_src_id = job_split[header_key_dict['gdc_src_id']]
-                location = job_split[header_key_dict['location']]
-                if cat == 'mkdup_needed':
-                    setup_job(db_cred_path, json_template_path, git_cwl_hash, node_json_dir, s3_load_bucket, scratch_dir,
-                              slurm_template_path, thread_count, alignment_last_step, cghub_id, gdc_id, gdc_src_id, location)
+                s3_location = job_split[header_key_dict['location']]
+                gdc_realign_file_name = os.path.basename(s3_location)
+                bam_file_name = gdc_realign_file_name.replace('_gdc_realn','')
+                if cat == 'realign_needed':
+                    setup_job(bam_file_name, db_table_name, node_json_dir, repo_hash, s3_load_bucket, scratch_dir,
+                              json_template_path, slurm_template_path)
                 
 
 if __name__=='__main__':
