b682eaa011dfdccdd0e5ea920fe4c08d8fbafffc
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-10-11 14:37:19 -0500

    wgs_330 -> wgs_753

--- workflows/dnaseq/salt.sh
+++ workflows/dnaseq/salt.sh
@@ -3,16 +3,16 @@ salt -G 'cluster_name:WOLVERINE' cmd.run runas=ubuntu "sed -i 's/kh11-9.osdc.io/
 
 # on workstation, prep cwl, slurm and json job files
 workon p3
-mkdir wgs_330
-mkdir wgs_330_slurm
-mkdir wgs_330_json
-cd wgs_330
-python ~/code/cocleaning-cwl/workflows/dnaseq/create_jobs_from_templates.py --db_table_name wgs_330_status --job_table_path ~/qcfail_realign_table.txt --json_template_path ~/code/cocleaning-cwl/workflows/dnaseq/runner_template.json --node_json_dir /home/ubuntu/wgs_330_json --repo_hash 5ac52b9655e14cad425274010d871bf6105babda --resource_core_count 40 --resource_disk_bytes 1539316278886 --resource_memory_bytes 42949672960  --s3_load_bucket s3://tcga_wgs_alignment_4 --scratch_dir /mnt/SCRATCH --slurm_template_path ~/code/cocleaning-cwl/workflows/dnaseq/slurm_template.sh
-mv *.json ../wgs_330_json
-mv *.sh ../wgs_330_slurm
+mkdir wgs_753
+mkdir wgs_753_slurm
+mkdir wgs_753_json
+cd wgs_753
+python ~/code/cocleaning-cwl/workflows/dnaseq/create_jobs_from_templates.py --db_table_name wgs_753_status --job_table_path ~/qcfail_realign_table.txt --json_template_path ~/code/cocleaning-cwl/workflows/dnaseq/runner_template.json --node_json_dir /home/ubuntu/wgs_753_json --repo_hash 7dd74936770bd9b2f0a968755b65ed8c4e48ae5e --resource_core_count 40 --resource_disk_bytes 1539316278886 --resource_memory_bytes 42949672960  --s3_load_bucket s3://tcga_wgs_alignment_4 --scratch_dir /mnt/SCRATCH --slurm_template_path ~/code/cocleaning-cwl/workflows/dnaseq/slurm_template.sh
+mv *.json ../wgs_753_json
+mv *.sh ../wgs_753_slurm
 cd ../
-rmdir wgs_330
-rsync -av --progress wgs_330* jer:/mnt/SCRATCH/
+rmdir wgs_753
+rsync -av --progress wgs_753* jer:/mnt/SCRATCH/
 rsync -av --progress ~/code/cocleaning-cwl jer:/mnt/SCRATCH/
 
 
@@ -24,14 +24,14 @@ cd ../
 s3cmd -c ~/.s3cfg.ceph --recursive put cocleaning-cwl s3://bioinformatics_scratch/jhsavage_salt/
 
 #push json to object store
-s3cmd -c ~/.s3cfg.ceph --recursive put wgs_330_json s3://bioinformatics_scratch/jhsavage_salt/
+s3cmd -c ~/.s3cfg.ceph --recursive put wgs_753_json s3://bioinformatics_scratch/jhsavage_salt/
 
 #put cwltool to object store
 wget https://github.com/jeremiahsavage/cwltool/archive/1.0_gdc_e.tar.gz
 s3cmd -c ~/.s3cfg.ceph put 1.0_gdc_e.tar.gz s3://bioinformatics_scratch/jhsavage_salt/
 
 
-salt -G 'cluster_name:WOLVERINE' cmd.run runas=ubuntu 'rm -rf /mnt/SCRATCH/wgs_330 /mnt/SCRATCH/read_write /mnt/SCRATCH/somaticmafpon /mnt/SCRATCH/cache /mnt/SCRATCH/tmp /mnt/SCRATCH/reference /mnt/SCRATCH/slurm*.out'
+salt -G 'cluster_name:WOLVERINE' cmd.run runas=ubuntu 'rm -rf /mnt/SCRATCH/wgs_753 /mnt/SCRATCH/read_write /mnt/SCRATCH/somaticmafpon /mnt/SCRATCH/cache /mnt/SCRATCH/tmp /mnt/SCRATCH/reference /mnt/SCRATCH/slurm*.out'
 salt -G 'cluster_name:WOLVERINE' cmd.run runas=ubuntu 'rm -rf /mnt/SCRATCH/*'
 salt -G 'cluster_name:WOLVERINE' cmd.run runas=ubuntu 'rm -rf /home/ubuntu/cocleaning-cwl /home/ubuntu/aws_jhsavage_credentials /home/ubuntu/jhsavage_endpoint.json /home/ubuntu/rw_json'
 salt -G 'cluster_name:WOLVERINE' cmd.run runas=ubuntu 'rm -rf /home/ubuntu/.virtualenvs'
