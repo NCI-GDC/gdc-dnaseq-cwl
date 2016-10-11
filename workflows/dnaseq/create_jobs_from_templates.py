885a5b0cf5335a802775eca80456113f3b22debe
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-10-11 14:49:56 -0500

    update for wgs753

--- workflows/dnaseq/create_jobs_from_templates.py
+++ workflows/dnaseq/create_jobs_from_templates.py
@@ -7,7 +7,6 @@ import os
 import sys
 import uuid
 
-
 def read_header(header_line):
     header_split = header_line.strip().split('\t')
     header_key_dict = dict()
@@ -15,38 +14,7 @@ def read_header(header_line):
         header_key_dict[header_column] = i
     return header_key_dict
 
-def get_parameter_from_file_name(file_name, parameter):
-    import json
-    import requests
-    endpoint = 'https://gdc-api.nci.nih.gov/legacy/files'
-    filter = {
-        "op": "in",
-        "content": {
-            "field": "file_name",
-            "value": [
-                file_name
-            ]
-        }
-    }
-    parameters = {'filters':json.dumps(filter)}
-    response = requests.get(endpoint, params = parameters)
-    if response.status_code == 200:
-        response_data = response.json()['data']
-        count = response_data['pagination']['count']
-        if count == 0:
-            return None
-        if count > 1:
-            print('file_name: %s' % file_name)
-            print('response_data:%s' % response_data)
-            print('count > 1: %s' % count)
-            sys.exit(1)
-        else:
-            parameter_value = response_data['hits'][0][parameter]
-            return parameter_value
-    sys.exit(1)
-    return
-
-def generate_runner(bam_file_name, db_table_name, gdc_src_id, job_uuid, repo_hash,
+def generate_runner(db_table_name, gdc_src_id, job_uuid, repo_hash,
                     resource_core_count, resource_disk_bytes, resource_memory_bytes,
                     s3_load_bucket, json_template_path):
     job_json = job_uuid + '.json'
@@ -54,15 +22,7 @@ def generate_runner(bam_file_name, db_table_name, gdc_src_id, job_uuid, repo_has
     with open(json_template_path, 'r') as read_open:
         for line in read_open:
             if 'XX_BAM_SIGNPOST_ID_XX' in line:
-                bam_signpost_id = get_parameter_from_file_name(bam_file_name, 'file_id')
-                if bam_signpost_id is None:
-                    bam_signpost_id = gdc_src_id
-                elif bam_signpost_id != gdc_src_id:
-                    print('bam_signpost_id: %s' % bam_signpost_id)
-                    print('gdc_src_id: %s' % gdc_src_id)
-                    print('bam_file_name: %s' % bam_file_name)
-                    sys.exit(1)
-                newline = line.replace('XX_BAM_SIGNPOST_ID_XX', bam_signpost_id)
+                newline = line.replace('XX_BAM_SIGNPOST_ID_XX', gdc_src_id)
                 f_open.write(newline)
             elif 'XX_DB_TABLE_NAME_XX' in line:
                 newline = line.replace('XX_DB_TABLE_NAME_XX', db_table_name)
@@ -84,7 +44,7 @@ def generate_runner(bam_file_name, db_table_name, gdc_src_id, job_uuid, repo_has
     f_open.close()
     return
 
-def generate_slurm(bam_file_name, db_table_name, gdc_src_id, job_uuid, node_json_dir,
+def generate_slurm(db_table_name, gdc_src_id, job_uuid, node_json_dir,
                    resource_core_count, resource_disk_bytes, resource_memory_bytes,
                    repo_hash, scratch_dir, slurm_template_path):
     job_slurm = job_uuid + '.sh'
@@ -92,15 +52,7 @@ def generate_slurm(bam_file_name, db_table_name, gdc_src_id, job_uuid, node_json
     with open(slurm_template_path, 'r') as read_open:
         for line in read_open:
             if 'XX_BAM_SIGNPOST_ID_XX' in line:
-                bam_signpost_id = get_parameter_from_file_name(bam_file_name, 'file_id')
-                if bam_signpost_id is None:
-                    bam_signpost_id = gdc_src_id
-                elif bam_signpost_id != gdc_src_id:
-                    print('bam_signpost_id: %s' % bam_signpost_id)
-                    print('gdc_src_id: %s' % gdc_src_id)
-                    print('bam_file_name: %s' % bam_file_name)
-                    sys.exit(1)
-                newline = line.replace('XX_BAM_SIGNPOST_ID_XX', bam_signpost_id)
+                newline = line.replace('XX_BAM_SIGNPOST_ID_XX', gdc_src_id)
                 f_open.write(newline)
             elif 'XX_DB_TABLE_NAME_XX' in line:
                 newline = line.replace('XX_DB_TABLE_NAME_XX', db_table_name)
@@ -134,15 +86,15 @@ def generate_slurm(bam_file_name, db_table_name, gdc_src_id, job_uuid, node_json
     f_open.close()
     return
 
-def setup_job(bam_file_name, db_table_name, gdc_src_id, node_json_dir, repo_hash,
+def setup_job(db_table_name, gdc_src_id, node_json_dir, repo_hash,
               resource_core_count, resource_disk_bytes, resource_memory_bytes,
               s3_load_bucket, scratch_dir, json_template_path, slurm_template_path):
     job_uuid = str(uuid.uuid4())
 
-    generate_runner(bam_file_name, db_table_name, gdc_src_id, job_uuid, repo_hash,
+    generate_runner(bam_signpost_id, db_table_name, gdc_src_id, job_uuid, repo_hash,
                     resource_core_count, resource_disk_bytes, resource_memory_bytes,
                     s3_load_bucket, json_template_path)
-    generate_slurm(bam_file_name, db_table_name, gdc_src_id, job_uuid, node_json_dir,
+    generate_slurm(bam_signpost_id, db_table_name, gdc_src_id, job_uuid, node_json_dir,
                    resource_core_count, resource_disk_bytes, resource_memory_bytes,
                    repo_hash, scratch_dir, slurm_template_path)
     return
@@ -211,17 +163,16 @@ def main():
 
     with open(job_table_path, 'r') as job_table_open:
         for job_line in job_table_open:
-            if job_line.startswith('program'):
+            if job_line.startswith('imported_gdc_id'):
                 header_key_dict = read_header(job_line)
             else:
                 job_split = job_line.strip().split('\t')
-                cat = job_split[header_key_dict['cat']]
-                gdc_src_id = job_split[header_key_dict['gdc_src_id']]
-                s3_location = job_split[header_key_dict['location']]
-                gdc_realign_file_name = os.path.basename(s3_location)
-                bam_file_name = gdc_realign_file_name.replace('_gdc_realn','')
+                gdc_src_id = job_split[header_key_dict['imported_gdc_id']]
+                imported_filesize = job_split[header_key_dict['imported_filesize']]
+                size_gb = job_split[header_key_dict['size_gb']]
+                slurm_core = job_split[header_key_dict['slurm_core']]
                 if cat == 'realign_needed':
-                    setup_job(bam_file_name, db_table_name, gdc_src_id, node_json_dir, repo_hash,
+                    setup_job(db_table_name, gdc_src_id, node_json_dir, repo_hash,
                               resource_core_count, resource_disk_bytes, resource_memory_bytes,
                               s3_load_bucket, scratch_dir, json_template_path, slurm_template_path)
                 
