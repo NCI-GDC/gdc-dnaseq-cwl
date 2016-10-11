d6d56783ac5835124c4ef7cae19c1eb2b8deb7b9
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-10-11 16:25:24 -0500

    proper --node_json_dir

--- workflows/dnaseq/salt.sh
+++ workflows/dnaseq/salt.sh
@@ -11,7 +11,7 @@ mkdir wgs_753_cl
 mkdir wgs_753_cl_slurm
 mkdir wgs_753_cl_json
 cd wgs_753_cl
-python ~/code/cocleaning-cwl/workflows/dnaseq/create_jobs_from_templates.py --db_table_name wgs_753_status --job_table_path ~/wgs753_cl.txt --json_template_path ~/code/cocleaning-cwl/workflows/dnaseq/runner_template.json --node_json_dir /home/ubuntu/wgs_753_json --repo_hash 46645d4da83a81d137b7dfdbaceea5e9755757a4 --resource_core_count 40 --resource_disk_bytes 1539316278886 --resource_memory_bytes 42949672960  --s3_load_bucket s3://tcga_wgs_alignment_4 --scratch_dir /mnt/SCRATCH --slurm_template_path ~/code/cocleaning-cwl/workflows/dnaseq/slurm_template.sh
+python ~/code/cocleaning-cwl/workflows/dnaseq/create_jobs_from_templates.py --db_table_name wgs_753_status --job_table_path ~/wgs753_cl.txt --json_template_path ~/code/cocleaning-cwl/workflows/dnaseq/runner_template.json --node_json_dir /home/ubuntu/wgs_753_cl_json --repo_hash 016704f2c2c324d0f81d527c60097c3db0340bc9 --resource_core_count 40 --resource_disk_bytes 1539316278886 --resource_memory_bytes 42949672960  --s3_load_bucket s3://tcga_wgs_alignment_4 --scratch_dir /mnt/SCRATCH --slurm_template_path ~/code/cocleaning-cwl/workflows/dnaseq/slurm_template.sh
 mv *.json ../wgs_753_cl_json
 mv *.sh ../wgs_753_cl_slurm
 cd ../
@@ -22,33 +22,35 @@ mkdir wgs_753_dp
 mkdir wgs_753_dp_slurm
 mkdir wgs_753_dp_json
 cd wgs_753_dp
-python ~/code/cocleaning-cwl/workflows/dnaseq/create_jobs_from_templates.py --db_table_name wgs_753_status --job_table_path ~/wgs753_dp.txt --json_template_path ~/code/cocleaning-cwl/workflows/dnaseq/runner_template.json --node_json_dir /home/ubuntu/wgs_753_json --repo_hash 46645d4da83a81d137b7dfdbaceea5e9755757a4 --resource_core_count 40 --resource_disk_bytes 1539316278886 --resource_memory_bytes 42949672960  --s3_load_bucket s3://tcga_wgs_alignment_4 --scratch_dir /mnt/SCRATCH --slurm_template_path ~/code/cocleaning-cwl/workflows/dnaseq/slurm_template.sh
+python ~/code/cocleaning-cwl/workflows/dnaseq/create_jobs_from_templates.py --db_table_name wgs_753_status --job_table_path ~/wgs753_dp.txt --json_template_path ~/code/cocleaning-cwl/workflows/dnaseq/runner_template.json --node_json_dir /home/ubuntu/wgs_753_dp_json --repo_hash 016704f2c2c324d0f81d527c60097c3db0340bc9 --resource_core_count 40 --resource_disk_bytes 1539316278886 --resource_memory_bytes 42949672960  --s3_load_bucket s3://tcga_wgs_alignment_4 --scratch_dir /mnt/SCRATCH --slurm_template_path ~/code/cocleaning-cwl/workflows/dnaseq/slurm_template.sh
 mv *.json ../wgs_753_dp_json
 mv *.sh ../wgs_753_dp_slurm
 cd ../
 rmdir wgs_753_dp
 
 
-rsync -av --progress wgs_753* jer:/mnt/SCRATCH/
-rsync -av --progress ~/code/cocleaning-cwl jer:/mnt/SCRATCH/
+rsync -av --progress wgs_753_* jer:/mnt/SCRATCH/
+cp -arf ~/code/cocleaning-cwl ~/
+cd ~/cocleaning-cwl/
+rm -rf .git
+cd  ~/
+rsync -av --progress ~/cocleaning-cwl jer:/mnt/SCRATCH/
 
 
 #push cocleaning-cwl to any node, and place on object store
 ssh jer
 cd /mnt/SCRATCH/cocleaning-cwl
-rm -rf .git
-cd ../
 s3cmd -c ~/.s3cfg.ceph --recursive put cocleaning-cwl s3://bioinformatics_scratch/jhsavage_salt/
 
 #push json to object store
-s3cmd -c ~/.s3cfg.ceph --recursive put wgs_753_json s3://bioinformatics_scratch/jhsavage_salt/
+s3cmd -c ~/.s3cfg.ceph --recursive put wgs_753_cl_json s3://bioinformatics_scratch/jhsavage_salt/
+s3cmd -c ~/.s3cfg.ceph --recursive put wgs_753_dp_json s3://bioinformatics_scratch/jhsavage_salt/
 
 #put cwltool to object store
 wget https://github.com/jeremiahsavage/cwltool/archive/1.0_gdc_e.tar.gz
 s3cmd -c ~/.s3cfg.ceph put 1.0_gdc_e.tar.gz s3://bioinformatics_scratch/jhsavage_salt/
 
 
-salt -G 'cluster_name:WOLVERINE' cmd.run runas=ubuntu 'rm -rf /mnt/SCRATCH/wgs_753 /mnt/SCRATCH/read_write /mnt/SCRATCH/somaticmafpon /mnt/SCRATCH/cache /mnt/SCRATCH/tmp /mnt/SCRATCH/reference /mnt/SCRATCH/slurm*.out'
 salt -G 'cluster_name:WOLVERINE' cmd.run runas=ubuntu 'rm -rf /mnt/SCRATCH/*'
 salt -G 'cluster_name:WOLVERINE' cmd.run runas=ubuntu 'rm -rf /home/ubuntu/cocleaning-cwl /home/ubuntu/aws_jhsavage_credentials /home/ubuntu/jhsavage_endpoint.json /home/ubuntu/rw_json'
 salt -G 'cluster_name:WOLVERINE' cmd.run runas=ubuntu 'rm -rf /home/ubuntu/.virtualenvs'
@@ -59,8 +61,7 @@ salt -G 'cluster_name:WOLVERINE' cmd.run 'echo "Dpkg::Options {
 };
 " >> /etc/apt/apt.conf.d/50unattended-upgrades'
 salt -G 'cluster_name:WOLVERINE' cmd.run 'apt-get update &&  apt-get install python-dev libffi-dev libssl-dev htop s3cmd -y && apt-get clean'
-salt -G 'cluster_name:WOLVERINE' cmd.run 'echo "172.17.52.27 signpost.service.consul" >> /etc/hosts'
-salt -G 'cluster_name:WOLVERINE' cmd.run "sed -i 's/172.17.52.27/172.17.12.36/g' /etc/hosts"
+salt -G 'cluster_name:WOLVERINE' cmd.run 'echo "172.17.12.36 signpost.service.consul" >> /etc/hosts'
 
 
 salt -G 'cluster_name:WOLVERINE' cmd.run 'apt-get update &&  apt-get upgrade -y && apt-get autoremove'
