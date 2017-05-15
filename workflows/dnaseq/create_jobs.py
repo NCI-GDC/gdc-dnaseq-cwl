b0ca56ff110c767fb4dc57dab6ec64ce3c1c4969
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2017-05-15 17:47:33 -0500

    basic functionality works

--- workflows/dnaseq/create_jobs.py
+++ workflows/dnaseq/create_jobs.py
@@ -68,7 +68,7 @@ def generate_runner(job_json_file, queue_data, runner_text):
             except KeyError:
                 continue
     setattr(runner_template, 'thread_count', '8')
-    with open(job_json, 'w') as f_open:
+    with open(job_json_file, 'w') as f_open:
         json.dump(runner_template, f_open, sort_keys=True, indent=4)
 
     return
@@ -88,7 +88,7 @@ def generate_slurm(job_slurm_file, queue_data, slurm_template_text):
 def setup_job(queue_item):
     job_json_file = '/'.join((queue_item['job_creation_uuid'], 'cwl', queue_item['input_bam_gdc_id'] + '_alignment.json'))
     job_slurm_file = '/'.join((queue_item['job_creation_uuid'], 'slurm', queue_item['input_bam_gdc_id'] + '_alignment.sh'))
-    json_uri = '/'.join((queue_item['runner_job_base_uri'], job_json))
+    json_uri = '/'.join((queue_item['runner_job_base_uri'], job_json_file))
 
     runner_json_template_text = fetch_text(queue_item['runner_json_template_uri'])
     slurm_template_text = fetch_text(queue_item['slurm_template_uri'])
@@ -96,22 +96,22 @@ def setup_job(queue_item):
     slurm_core = SLURM_CORE # will eventually be decided by cwl engine at run time per step
     slurm_mem_megabytes = SLURM_MEM # make a model
     slurm_disk_gigabytes = math.ceil(10 * (int(queue_item['input_bam_file_size']) / (1000**3))) #use readgroup, will eventually be decided by cwl engine at run time per step
-    queue_item.slurm_core = slurm_core_count
-    queue_item.slurm_mem_megabytes = slurm_mem_megabytes
-    queue_item.slurm_disk_gigabytes = slurm_disk_gigabytes
+    queue_item['slurm_core'] = str(slurm_core)
+    queue_item['slurm_mem_megabytes'] = str(slurm_mem_megabytes)
+    queue_item['slurm_disk_gigabytes'] = str(slurm_disk_gigabytes)
 
     runner_cwl_branch = ''
-    queue_item.runner_cwl_branch = runner_cwl_branch
+    queue_item['runner_cwl_branch'] = runner_cwl_branch
     runner_cwl_repo = ''
-    queue_item.runner_cwl_repo = runner_cwl_repo
+    queue_item['runner_cwl_repo'] = runner_cwl_repo
     runner_job_branch = ''
-    queue_item.runner_job_branch = runner_job_branch
-    runner_job_cwl_uri = ''
-    queue_item.runner_job_cwl_uri = runner_job_cwl_uri
+    queue_item['runner_job_branch'] = runner_job_branch
+    runner_job_cwl_uri = json_uri
+    queue_item['runner_job_cwl_uri'] = runner_job_cwl_uri
     runner_job_repo = ''
-    queue_item.runner_job_repo = runner_job_repo
+    queue_item['runner_job_repo'] = runner_job_repo
     runner_job_slurm_uri = ''
-    queue_item.runner_job_slurm_uri = runner_job_slurm_uri
+    queue_item['runner_job_slurm_uri'] = runner_job_slurm_uri
 
 
     generate_runner(job_json_file, queue_item, runner_json_template_text)
@@ -152,7 +152,7 @@ def main():
     with open(queue_json, 'r') as f:
         queue_dict = json.loads(f.read())
     for queue_item in queue_dict:
-        queue_item.job_creation_uuid = job_creation_uuid
+        queue_item['job_creation_uuid'] = job_creation_uuid
         setup_job(queue_item)
     return
 
