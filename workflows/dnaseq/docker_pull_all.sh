7220aeece3457b8f3cf3d2f2a037af809ae74149
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2017-05-16 20:17:41 -0500

    test each tool for latest sha256 docker Id

--- workflows/dnaseq/docker_pull_all.sh
+++ workflows/dnaseq/docker_pull_all.sh
@@ -1,6 +1,25 @@
-cwltool --pack runner_wgs.cwl > runner_wgs_pack.cwl
+#set -e
+cwltool --pack workflows/dnaseq/runner_wgs.cwl > runner_wgs_pack.cwl
 docker_repos=($(grep dockerPull runner_wgs_pack.cwl | sort | uniq | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//' | awk -F ":" '{print $1}' | grep ncigdc))
+# for docker_repo in "${docker_repos[@]}"
+# do
+#     docker pull ${docker_repo}:latest
+# done
+
 for docker_repo in "${docker_repos[@]}"
 do
-    docker pull ${docker_repo}:latest
+    docker inspect ${docker_repo}:latest | grep "Id\":" | awk -F ":" '{print $3}' | sed -e 's/",$//'
+done
+
+rm runner_wgs_pack.cwl
+
+for docker_repo in "${docker_repos[@]}"
+do
+    file_names=($(grep -lr ${docker_repo} *))
+    for file_name in "${file_names[@]}"
+    do
+        echo ${file_name}
+        did=$(docker inspect ${docker_repo}:latest | grep "Id\":" | awk -F ":" '{print $3}' | sed -e 's/",$//')
+        grep ${docker_repo}:${did} ${file_name}
+    done
 done
