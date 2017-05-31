f28dea05e02f2c85dfe327407a9f8f4005e35b1e
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2017-05-30 19:10:55 -0500

    enable

--- workflows/dnaseq/docker_provenance.sh
+++ workflows/dnaseq/docker_provenance.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 #set -e
-#cwltool --pack workflows/dnaseq/runner_wgs.cwl > runner_wgs_pack.cwl
+cwltool --pack workflows/dnaseq/runner_wgs.cwl > runner_wgs_pack.cwl
 docker_repos=($(grep dockerPull runner_wgs_pack.cwl | sort | uniq | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//' | awk -F ":" '{print $1}' | grep ncigdc))
 for docker_repo in "${docker_repos[@]}"
 do
     docker pull ${docker_repo}:latest
 done
 
-#rm runner_wgs_pack.cwl
+rm runner_wgs_pack.cwl
 
 for docker_repo in "${docker_repos[@]}"
 do
