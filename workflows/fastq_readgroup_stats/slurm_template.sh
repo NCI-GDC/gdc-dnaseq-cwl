0c64722c44618bbdc521e0494024fda5a593b6b9
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2017-08-23 13:06:44 -0500

    update variable names

--- workflows/fastq_readgroup_stats/slurm_template.sh
+++ workflows/fastq_readgroup_stats/slurm_template.sh
@@ -13,9 +13,9 @@ VIRTUALENV_NAME=${xx_virtualenv_name_xx}
 ##JOB VARIABLE
 CWL_GET_WORKFLOW_URL=${xx_cwl_get_workflow_url_xx}
 CWL_JOB_URL=${xx_cwl_runner_job_url_xx}
-CWL_WORKFLOW_GIT_URL=${xx_cwl_workflow_git_url_xx}
+CWL_RUNNER_LOCALPATH=${xx_cwl_runner_localpath_xx}
+CWL_WORKFLOW_GIT_REPO=${xx_cwl_workflow_git_repo_xx}
 CWL_WORKFLOW_GIT_HASH=${xx_cwl_workflow_git_hash_xx}
-CWL_WORKFLOW_PATH=${xx_cwl_workflow_path_xx}
 JOB_ID=${xx_job_id_xx}
 
 function activate_virtualenv()
@@ -29,7 +29,7 @@ function activate_virtualenv()
 function runner()
 {
     local cwl_job_url=${1}
-    local cwl_runner_path=${2}
+    local cwl_runner_localpath=${2}
     local job_dir=${3}
 
     local cache_dir=${job_dir}/cache
@@ -39,14 +39,14 @@ function runner()
     mkdir -p ${tmp_dir}
 
     export http_proxy=http://cloud-proxy:3128; export https_proxy=http://cloud-proxy:3128;
-    cwltool --debug --rm-tmpdir --rm-container --tmp-outdir-prefix ${cache_dir} --tmpdir-prefix ${tmp_dir} --custom-net bridge --outdir ${job_dir} ${cwl_runner_path} ${cwl_job_url}
+    cwltool --debug --rm-tmpdir --rm-container --tmp-outdir-prefix ${cache_dir} --tmpdir-prefix ${tmp_dir} --custom-net bridge --outdir ${job_dir} ${cwl_runner_localpath} ${cwl_job_url}
 }
 
 function get_workflow()
 {
     local cwl_get_workflow_url=${1}
     local cwl_workflow_git_hash=${2}
-    local cwl_workflow_git_url=${3}
+    local cwl_workflow_git_repo=${3}
     local job_dir=${4}
 
     local cache_dir=${job_dir}/cache
@@ -56,16 +56,16 @@ function get_workflow()
     mkdir -p ${tmp_dir}
 
     export http_proxy=http://cloud-proxy:3128; export https_proxy=http://cloud-proxy:3128;
-    cwltool --debug --rm-tmpdir --rm-container --tmp-outdir-prefix ${cache_dir} --tmpdir-prefix ${tmp_dir} --custom-net bridge --outdir ${job_dir} ${cwl_get_workflow_url} --git_hash ${cwl_workflow_git_hash} --repo_url ${cwl_workflow_git_url}
+    cwltool --debug --rm-tmpdir --rm-container --tmp-outdir-prefix ${cache_dir} --tmpdir-prefix ${tmp_dir} --custom-net bridge --outdir ${job_dir} ${cwl_get_workflow_url} --git_hash ${cwl_workflow_git_hash} --repo_url ${cwl_workflow_git_repo}
 }
 
 function main()
 {
     local cwl_get_workflow_url=${CWL_GET_WORKFLOW_URL}
     local cwl_job_url=${CWL_JOB_URL}
-    local cwl_workflow_git_url=${CWL_WORKFLOW_GIT_URL}
+    local cwl_runner_localpath=${CWL_RUNNER_LOCALPATH}
+    local cwl_workflow_git_repo=${CWL_WORKFLOW_GIT_REPO}
     local cwl_workflow_git_hash=${CWL_WORKFLOW_GIT_HASH}
-    local cwl_workflow_path=${CWL_WORKFLOW_PATH}
     local job_id=${JOB_ID}
     local scratch_dir=${SCRATCH_DIR}
     local virtualenv_name=${VIRTUALENV_NAME}
@@ -74,7 +74,7 @@ function main()
 
     activate_virtualenv ${virtualenv_name}
     pull_workflow ${cwl_get_workflow_url} ${cwl_workflow_git_hash} ${cwl_workflow_git_url} ${job_dir}
-    runner ${cwl_job_url} ${cwl_workflow_path} ${job_dir}
+    runner ${cwl_job_url} ${cwl_runner_localpath} ${job_dir}
     if [ $? -ne 0 ]
     then
         echo FAIL_RUNNER
